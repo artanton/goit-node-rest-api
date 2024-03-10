20f9450ca149153d543d2bd5a1da5bbf6292cd44
artanton <kabletrade@gmail.com>
2024-03-10 13:47:27 +0200

    hw-3mode-1

--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -1,22 +1,25 @@
 import express from "express";
+import contactsController from "../controllers/contactsControllers.js";
 import {
-  getAllContacts,
-  getOneContact,
-  deleteContact,
-  createContact,
-  updateContact,
-} from "../controllers/contactsControllers.js";
+  createContactSchema,
+  updateContactSchema,
+} from "../schemas/contactsSchemas.js";
+import validateBody from "../decorators/validateBody.js";
+import isValidId from "../middlewares/isValidId.js";
 
 const contactsRouter = express.Router();
 
-contactsRouter.get("/", getAllContacts);
+contactsRouter.get("/", contactsController.getAllContacts);
 
-contactsRouter.get("/:id", getOneContact);
+contactsRouter.get("/:id", isValidId, contactsController.getOneContact);
 
-contactsRouter.delete("/:id", deleteContact);
+contactsRouter.post("/",
+  validateBody(createContactSchema),
+  contactsController.createContact
+);
 
-contactsRouter.post("/", createContact);
+contactsRouter.put("/:id", isValidId, contactsController.updateContact);
 
-contactsRouter.put("/:id", updateContact);
+contactsRouter.delete("/:id", isValidId, contactsController.deleteContact);
 
 export default contactsRouter;
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,15 +1,15 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().required(),
-    phone: Joi.string().required(),
-
-})
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+  favorite: Joi.boolean().required(),
+});
 
 export const updateContactSchema = Joi.object({
-    name: Joi.string(),
-    email: Joi.string(),
-    phone: Joi.string(),
-
-})
+  name: Joi.string(),
+  email: Joi.string(),
+  phone: Joi.string(),
+  favorite: Joi.boolean(),
+});
